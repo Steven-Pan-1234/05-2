a3b6cd47c61de0c7b6ba74e8a045a81e5995fba8
StevenPan1234 <>
2021-05-12 00:39:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('button').addEventListener('click', ws)
+let age = 0
+let date = 'week'
+function ws () {
+  age = document.getElementById('age').value
+  age = parseInt(age)
+  date = document.getElementById('date').value
+  if ( (age >= 0 && age <= 18) && (date !== 'sunday' && date !== 'saturday') ) {
+    document.getElementById('p').innerHTML = 'go to school'
+  }
+  else if ( (age > 18) && (date !== 'sunday' && date !== 'saturday') ) {
+    document.getElementById('p').innerHTML = 'go to work'
+  }
+  else if ( (age > 0) && (date == 'sunday' || date == 'saturday') ) 
+  {
+    document.getElementById('p').innerHTML = 'Time to relax for the weekend'
+    }
+}
