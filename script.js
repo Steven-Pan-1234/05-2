821026b3c01d4a94666032ba821201d5aa4b89af
StevenPan1234 <>
2021-05-12 05:26:45 +0000

    fix

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function ws () {
   age = document.getElementById('age').value
   age = parseInt(age)
   date = document.getElementById('date').value
-  if ( (age >= 0 && age <= 18) && (date !== 'sunday' && date !== 'saturday') ) {
+  if ((age >= 0 && age <= 18) && (date !== 'sunday' && date !== 'saturday')) {
     document.getElementById('p').innerHTML = 'go to school'
-  }
-  else if ( (age > 18) && (date !== 'sunday' && date !== 'saturday') ) {
+    }
+  else if ((age > 18) && (date !== 'sunday' && date !== 'saturday')) {
     document.getElementById('p').innerHTML = 'go to work'
-  }
-  else if ( (age > 0) && (date == 'sunday' || date == 'saturday') ) 
+    }
+  else if ((age > 0) && (date === 'sunday' || date === 'saturday')) 
   {
     document.getElementById('p').innerHTML = 'Time to relax for the weekend'
     }
